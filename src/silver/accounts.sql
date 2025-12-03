15499478faf8fdac80602fcd2348e3c1c87231a1
isabelalobatobraga <isabelalobatob@hotmail.com>
2025-12-03 11:44:09 +0000

    Insert fact tables

--- src/silver/accounts.sql
+++ src/silver/accounts.sql
@@ -14,33 +14,50 @@ WITH normalized AS (
         -- (values are NOT persisted in clear form)
         REGEXP_REPLACE(TRIM(account_branch), '[^0-9]', '') AS branch_norm,
         REGEXP_REPLACE(TRIM(account_number), '[^0-9]', '') AS number_norm,
-        REGEXP_REPLACE(TRIM(account_check_digit), '[^0-9]', '') AS digit_norm,
-
-        current_timestamp() AS inserted_at
+        REGEXP_REPLACE(TRIM(account_check_digit), '[^0-9]', '') AS digit_norm
     FROM bronze.take_home_test.accounts
-)
+),
 
+masked AS (
+  SELECT
+      account_id,
+      customer_id,
+      created_at,
+      is_active,
+
+      -- Masked Account Branch without ever exposing the real value
+      -- Keep only the last digit visible (max length = 4)
+      CONCAT(
+          '***',
+          RIGHT(LPAD(branch_norm, 4, '0'), 1)
+      ) AS account_branch_masked,
+
+      -- Masked Account Numbber without ever exposing the real value
+      -- Keep only the last two digit visible (max length = 5)
+      CONCAT(
+          '***',
+          RIGHT(LPAD(number_norm, 5, '0'), 2)
+      ) AS account_number_masked,
+
+      -- Check digit: always fully masked (1 digit)
+      '*' AS account_check_digit_masked
+  FROM normalized
+)
 SELECT
     account_id,
     customer_id,
     created_at,
     is_active,
 
-    -- Masked Account Branch without ever exposing the real value
-    -- Keep only the last digit visible (max length = 4)
-    CONCAT(
-        '***',
-        RIGHT(LPAD(branch_norm, 4, '0'), 1)
-    ) AS account_branch_masked,
+    -- Identifies the account code format (future-proof)
+    'BR_BANKING' AS account_code_type,
 
-    -- Masked Account Numbber without ever exposing the real value
-    -- Keep only the last two digit visible (max length = 5)
+    -- Unified masked account code (Brazil format)
     CONCAT(
-        '***',
-        RIGHT(LPAD(number_norm, 5, '0'), 2)
-    ) AS account_number_masked,
+        account_branch_masked, '-',
+        account_number_masked, '-',
+        account_check_digit_masked
+    ) AS account_code,
 
-    -- Check digit: always fully masked (1 digit)
-    '*' AS account_check_digit_masked,
-    inserted_at
-FROM normalized
+    current_timestamp() AS inserted_at
+FROM masked;
